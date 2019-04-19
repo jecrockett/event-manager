50ce56fa3a58b9fef8b0c1894e536ed3c531955c
James Crockett <jcrockett@patientslikeme.com>
2019-04-19 12:38:54 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+events_json = ActiveSupport::JSON.decode(File.read('db/seeds/events.json'))
+events_json.each do |event|
+  Event.create!(event)
+end
